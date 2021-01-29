b88bdb9116f0328deb376e605fd2ec921407911c
Pierre Guillou <pierre.guillou@lip6.fr>
2021-01-29 20:21:14 +0100

    [tests] Also manage Python state files

--- tests/run.py
+++ tests/run.py
@@ -6,20 +6,32 @@ from paraview import simple
 
 def gen_screenshot(state):
     print(f"Processing {state}")
-    simple.LoadState(str(state))
     for i, view in enumerate(simple.GetViews()):
         simple.SaveScreenshot(f"{state}_{i}.png", view)
         print(f"{state}: view #{i} saved")
     simple.ResetSession()
 
 
+def process_psvm(state):
+    simple.LoadState(str(state))
+    gen_screenshot(state)
+
+
+def process_py(state):
+    with open(state, "r") as st:
+        exec(st.read())
+    gen_screenshot(state)
+
+
 def main():
     p = pathlib.Path(".") / "states"
-    for state in sorted(p.glob("*.pvsm")):
-        # keep instances isolated (fix segfaults)
-        p = multiprocessing.Process(target=gen_screenshot, args=(state,))
-        p.start()
-        p.join()
+    patterns = {"*.pvsm": process_psvm, "*.py": process_py}
+    for k, v in patterns.items():
+        for state in sorted(p.glob(k)):
+            # keep instances isolated (fix segfaults)
+            proc = multiprocessing.Process(target=v, args=(state,))
+            proc.start()
+            proc.join()
 
 
 if __name__ == "__main__":
