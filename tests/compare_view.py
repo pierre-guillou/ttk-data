461d7cce7370dd92f68172355e08d6cd92a76caa
Pierre Guillou <pierre.guillou@lip6.fr>
2021-03-07 19:39:08 +0100

    [tests] Replace OpenCV/Numpy with VTK/TTK

--- tests/compare_view.py
+++ tests/compare_view.py
@@ -1,22 +1,48 @@
 import argparse
 
-import cv2
-import numpy as np
+import topologytoolkit as ttk
+import vtk
 
 
 def main(f0, f1):
     # load the two input images
-    im0 = cv2.imread(f0)
-    im1 = cv2.imread(f1)
+    im0 = vtk.vtkPNGReader()
+    im0.SetFileName(f0)
+    im1 = vtk.vtkPNGReader()
+    im1.SetFileName(f1)
+    im1.Update()
     # convert the images to grayscale
-    gray0 = cv2.cvtColor(im0, cv2.COLOR_BGR2GRAY)
-    gray1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
-    # make sure the two images have the same dimensions
-    assert gray0.shape == gray1.shape
-    # compute the Mean Square Error between the two images
-    err = np.sum((gray0.astype("float") - gray1.astype("float")) ** 2)
-    err /= float(gray0.shape[0] * gray0.shape[1])
-    print(f"MSE: {err}")
+    calc0 = vtk.vtkArrayCalculator()
+    calc0.AddVectorArrayName("PNGImage")
+    calc0.SetFunction("mag(PNGImage)")
+    calc0.SetInputConnection(im0.GetOutputPort())
+    calc1 = vtk.vtkArrayCalculator()
+    calc1.AddVectorArrayName("PNGImage")
+    calc1.SetFunction("mag(PNGImage)")
+    calc1.SetInputConnection(im1.GetOutputPort())
+    # merge the two arrays with "Append Attributes"
+    ma = vtk.vtkMergeArrays()
+    ma.AddInputConnection(calc0.GetOutputPort())
+    ma.AddInputConnection(calc1.GetOutputPort())
+    ma.Update()
+    data = ma.GetOutput()
+    na = data.GetPointData().GetNumberOfArrays()
+    if na == 2:
+        # not enough arrays
+        print("Images have not the same dimension")
+        return
+    # compute the L2 distance between the two images
+    ldist = ttk.ttkLDistance()
+    ldist.SetInputConnection(ma.GetOutputPort())
+    ldist.SetInputArrayToProcess(
+        0, 0, 0, vtk.vtkDataObject.FIELD_ASSOCIATION_POINTS, "resultArray"
+    )
+    ldist.SetInputArrayToProcess(
+        1, 0, 0, vtk.vtkDataObject.FIELD_ASSOCIATION_POINTS, "resultArray_input_1"
+    )
+    ldist.Update()
+    print(ldist.GetErrorCode())
+    print(f"MSE: {ldist.Getresult()}")
 
 
 if __name__ == "__main__":
